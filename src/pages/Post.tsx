c521996479ca1278a223a1310ecf1ab3186f0906
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-09-27 10:32:44 -0500

    add react markdown, and start displaying comments

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,29 +1,70 @@
-import { IonPage, IonProgressBar } from "@ionic/react";
+import { IonAccordion, IonAccordionGroup, IonBackButton, IonButtons, IonContent, IonHeader, IonItem, IonList, IonPage, IonProgressBar } from "@ionic/react";
+import { AxiosError } from "axios";
+import ReactMarkdown from "react-markdown";
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
 import { getPost } from "../data/get-post";
-import { AxiosError } from "axios";
+import { Post as PostModel, PurpleReplies } from "../data/post-models";
 
 const Post: React.FC = () => {
   const { subreddit, postId } = useParams<{ subreddit: string, postId: string }>();
-  const { data, isLoading, isError, error } = useQuery(['post', postId], () => getPost(subreddit, postId))
+  const { data, isLoading, isError, error } = useQuery<PostModel[]>(['post', postId], () => getPost(subreddit, postId));
   
-  if (isLoading) return (
-    <IonPage>
-      <IonProgressBar type='indeterminate' />
-    </IonPage>
+  const IsLoading = () => <IonProgressBar type='indeterminate' />;
+  const IsError = () => <>{(error as AxiosError)?.message ?? 'Error loading post'}</>;
+
+  const normalizedData = () => {
+    if (!data) {
+      return [];
+    }
+    const [_, ...posts] = data;
+
+    return posts.map(p => p.data.children.map(c => (
+      {
+        comment: c.data.body ?? '',
+        replies: c.data.replies ?? []
+      }
+    ))).flat();
+  }
+
+  const Cards = () => (
+    <IonContent fullscreen>
+      <IonList>
+        {normalizedData().map((c, i) =>
+          <IonAccordionGroup key={i} value="open">
+            <IonAccordion value="open">
+              <IonItem slot="header">
+                <ReactMarkdown>{c.comment}</ReactMarkdown>
+              </IonItem>
+              <div className="ion-padding" slot="content">
+                <ul>
+                  {(c.replies as PurpleReplies).data?.children?.map((x, ii) => (
+                    x.kind === 'more' ? <li key={ii}>Read more</li> : <li key={ii}>{x.data.body}</li>
+                  ))}
+                </ul>
+              </div>
+            </IonAccordion>
+          </IonAccordionGroup>
+        )}
+      </IonList>
+    </IonContent>
   )
 
-  if (isError) return (
-    <IonPage>
-      {(error as AxiosError)?.message ?? 'Error loading post'}
-    </IonPage>
-  )
+  const Content = () => {
+    if (isLoading) return <IsLoading />
+    if (isError) return <IsError />
+
+    return <Cards />
+  }
 
   return (
     <IonPage>
-      {subreddit}/{postId}
-      {JSON.stringify(data)}
+      <IonHeader collapse="fade">
+        <IonButtons slot="start">
+          <IonBackButton text="Posts" defaultHref="/home"/>
+        </IonButtons>
+      </IonHeader>
+      <Content />
     </IonPage>
   )
 }
